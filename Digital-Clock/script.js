90af4dc18ee0fb5e20b78c3d9bd9f235186246dc
Manas Ranjan <manasrajan9852@gmai.com>
2025-06-14 13:10:36 +0530

    Added README file

---
+++ Digital-Clock/script.js
@@ -0,0 +1,14 @@
+const hourEl = document.querSelector('.hour');
+const minuteEl = document.querySelector(".minutes");
+const periodEl = document.querySelector(".period");
+const secondEl = document.querySelector(".seconds");
+
+const calendar = document.querSelector('.calendar')
+
+const clock = () => {
+    const today = new Date();
+    const hour = today.getHours();
+    const minute = today.getMinutes();
+    const seconds = today.getSeconds();
+    
+}
